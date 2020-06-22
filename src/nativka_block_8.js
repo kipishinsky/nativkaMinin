9f173007066dec453f0bbb9f490e22b12886d0bb
SS007 <rbkmoneyukk@bk.ru>
2020-06-22 18:47:47 +0400

    block 8 lesson 7 test

--- src/nativka_block_8.js
+++ src/nativka_block_8.js
@@ -1,282 +1,92 @@
-// вывести сообщение в алерте через 2 секунды
-/*setTimeout(function () {
-    alert('Hello')
-}, 2000)*/
-
-// через каждые 2 секунды выводить ++ counter
-/*var counter = 0;
-var interval = setInterval(function () {
-    console.log(++counter)
-}, 2000)*/
-
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 /*
-//остановить счетчик интервала
-clearInterval(interval)
+    Реализуйте класс MyPromise, который будет работать точно так же, как и promise.
+    Достаточно реализовать методы: then, catch, finally
+    Методы All, race делать не нужно
 */
 
-/* базовые составляющие асинхронности языка */
 
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// Старый поход CallBack   Старый поход CallBack   Старый поход CallBack   Старый поход CallBack   Старый поход CallBack   Старый поход CallBack
-
-// взаимодействие клиента и сервера
-// client => server => dataBase => server => client
-
-// client
+// так работают стандартные промисы
 /*
-console.log('Клиент: хочу получить список пользователей')
-console.log('...')
-*/
-
-    //server
-/*setTimeout(function() {
-    console.log('Сервер: Запрашиваю список пользователей в БД')
-    console.log('...')
-
-    // dataBase
-    setTimeout(function() {
-        console.log('БД: формирую список пользователей')
-        console.log('...')
-
-        // server
-        setTimeout(()=> {
-            console.log('Сервер: трансформирую данные для клиента ')
-            console.log('...')
-
-            setTimeout(function() {
-                console.log('Клиент: получил данные с сервера и отображаю их')
-                console.log('...')
-            }, 1000)
-        }, 500)
-    }, 500)
-}, 1000)*/
-
-
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// Как работают PROMISE      Как работают PROMISE      Как работают PROMISE      Как работают PROMISE      Как работают PROMISE
-// Promise (обычно их так и называют «промисы») – предоставляют удобный способ организации асинхронного кода.
-// https://learn.javascript.ru/promise
-// суть промисов - создаем какой то объект и оборачиваем в него какой то асинхронный код
-
-
-
-/*
-setTimeout(function() {
-    console.log('Сервер: Запрашиваю список пользователей в БД')
-    console.log('...')
-
-    // dataBase
-    setTimeout(function() {
-        console.log('БД: формирую список пользователей')
-        console.log('...')
-
-        // server
-        setTimeout(()=> {
-            console.log('Сервер: трансформирую данные для клиента ')
-            console.log('...')
-
-            setTimeout(function() {
-                console.log('Клиент: получил данные с сервера и отображаю их')
-                console.log('...')
-            }, 1000)
-        }, 500)
-    }, 500)
-}, 1000)
-*/
-
-/*
-console.log('Клиент: хочу получить список пользователей')
-console.log('...')
-
-
-let promise = new Promise(function (resolve, reject) { /!*
-    new Promise - создаем новый обхект
-    в параметры передаем функцию, которая выполняется сразу же, функция принимает 2 параметра
-    resolve - вызывается тогда, когда мы закончили какую то операцию успешно (без ошибки)
-    reject - вызывается тогда, когда мы закончили какую то операцию с ошибкой
-*!/
-    setTimeout(function(){
-        console.log('Сервер: Запрашиваю список пользователей в БД')
-        console.log('...')
-        resolve()
-    }, 1000)
-});
-
-// метод .then - соединение. последовательное соединение операций
-// сначала выполнится let promise (сервер), потом приклеится за ним код БД, потом приклеится за ним Сервер, потом Клиент
-promise.then(() => {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            //добавим слой данных
-            let users = [
-                {uid: 'id1', name: 'Max'},
-                {uid: 'id2', name: 'Elena'}
-            ];
-            console.log('БД: формирую список пользователей');
-            console.log('...');
-            resolve(users);
-        }, 500)
-    })
+let promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve({id:2})
+    }, 2000)
 })
-
-    .then((dbUsers) => { // dbUsers - называй как хочешь, в пропсах уже сидят данные users, которые мы передали из предыдущего метода
-        return new Promise((resolve, reject) => {
-            setTimeout(() => {
-                console.log('Сервер: трансформирую данные для клиента ')
-                console.log('...')
-                let users2 = dbUsers.map ( (user) => {
-                    return {
-                        id: user.id,
-                        firstName: user.name,
-                        timestamp: Date.now()
-                    }
-                })
-                resolve(users2)
-            }, 500)
-        })
-    })
-
-    .then((users2) => {
-        setTimeout(function() {
-            console.log('Клиент: получил данные с сервера и отображаю их', users2)
-            console.log('...')
-        }, 1000)
-    })
-
-        // метод catch(кетч) его мы вызываем в любом месте. он служит для того, чтобы мы обрабатывали ошибки
-    .catch((error) => {
-        console.error(error)
-    })
-
-        // метод finally (файнали) - вызывается тогда, когда закончились все асинхронные операции,
-        // даже если вызовится ошибся и какие то методы не отработают, метод файнали вызовится, тем самым показав
-        // что все отработало - сделать какое либо действие в любом случае
-    .finally (()=>{
+promise
+    .then(num => num *= 2)
+    .catch(err => console.log(err))
+    .then(num => num *= 3)
+    .finally(() => {
         console.log('finally')
     })
 */
 
 
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// Метод .Fetch    Метод .Fetch    Метод .Fetch    Метод .Fetch    Метод .Fetch    Метод .Fetch    Метод .Fetch    Метод .Fetch
-// делает запрос на удаленный сервер и возвращает нам promise(обещание c англ)
+// пишем свое
+class MyPromise {
+    constructor (callback) {
 
-// взаимодействие с сервером
-// тесты проводить можно на хэл ресурсе https://jsonplaceholder.typicode.com/
-// асинхронные запросы не приводят к перезагрузке страницы
+        this.onCatch = null
+        this.onFinally = null
+        this.thenCb = []
+        this.isRejected = false
 
+        function resolver(data) {
+            if (this.isRejected) {
+                return
+            }
 
-/*// достали кнопку
-document.querySelector('#load').addEventListener('click', load)
-
-//проверка работоспособности связки client => server
-function load() {
-
-    let url = 'https://jsonplaceholder.typicode.com/users'
-
-    fetch(url)
-        .then( function (response) {
-            return response.json()
-            //console.log('response', response) // при нажатии на кнопку получаем - Response {type: "cors", url: "https://jsonplaceholder.typicode.com/users", redirected: false, status: 200, ok: true, …}
-        })
-        .then((data) => {
-            let ul = document.querySelector('#list')
-
-            let html = data.map((i) => {
-                return '<li>' + i.id + ' ' + i.name + ' (' + i.email + ')</li>'
+            this.thenCb.forEach(cb => {
+                data = cb(data)
             })
-            //console.log('data', data) //data (10) [{…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}]
-            //console.log('data', html) // data (10) ["<li>1 Leanne Graham (Sincere@april.biz)</li>", "<li>2 Ervin Howell (Shanna@melissa.tv)</li>", "<li>3 Clementine Bauch (Nathan@yesenia.net)</li>", "<li>4 Patricia Lebsack (Julianne.OConner@kory.org)</li>", "<li>5 Chelsey Dietrich (Lucio_Hettinger@annie.ca)</li>", "<li>6 Mrs. Dennis Schulist (Karley_Dach@jasper.info)</li>", "<li>7 Kurtis Weissnat (Telly.Hoeger@billy.biz)</li>", "<li>8 Nicholas Runolfsdottir V (Sherwood@rosamond.me)</li>", "<li>9 Glenna Reichert (Chaim_McDermott@dana.io)</li>", "<li>10 Clementina DuBuque (Rey.Padberg@karina.biz)</li>"]
-            //console.log('data', html.join(' ')) //data <li>1 Leanne Graham (Sincere@april.biz)</li> <li>2 Ervin Howell (Shanna@melissa.tv)</li> <li>3 Clementine Bauch (Nathan@yesenia.net)</li> <li>4 Patricia Lebsack (Julianne.OConner@kory.org)</li> <li>5 Chelsey Dietrich (Lucio_Hettinger@annie.ca)</li> <li>6 Mrs. Dennis Schulist (Karley_Dach@jasper.info)</li> <li>7 Kurtis Weissnat (Telly.Hoeger@billy.biz)</li> <li>8 Nicholas Runolfsdottir V (Sherwood@rosamond.me)</li> <li>9 Glenna Reichert (Chaim_McDermott@dana.io)</li> <li>10 Clementina DuBuque (Rey.Padberg@karina.biz)</li>
-            ul.insertAdjacentHTML('afterbegin', html.join(' '))
-        })
-}*/
 
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//   Операторы: Async Await       Операторы: Async Await       Операторы: Async Await       Операторы: Async Await       Операторы: Async Await
-// https://habr.com/ru/company/ruvds/blog/414373/
-// es7
+            if (this.onFinally) {
+                this.onFinally()
+            }
+        }
+        function rejecter(error) {
+            this.isRejected = true
+            if (this.onCatch) {
+                this.onCatch(error)
+            }
+            if (this.onFinally) {
+                this.onFinally()
+            }
+        }
+        callback(resolver.bind(this),rejecter.bind(this))
+    }
 
-// document.querySelector('#load').addEventListener('click', load)
+    then(cb) {
+        this.thenCb.push(cb)
+        return this
+    }
 
-/*//рефакторим новым синтаксисом фукнцию load из предыдущего урока
-// async перед функцией, говорит нам о том, что функция асинхронная
-async function load() {
-    let url = 'https://jsonplaceholder.typicode.com/users'
-    let response = await fetch(url)
-    let data = await response.json()
-    let html = data.map( i => {
-        return '<li>' + i.id + ' ' + i.name + ' (' + i.email + ')</li>'
-    }).join(' ')
-    document.querySelector('#list').insertAdjacentHTML('afterbegin', html)
-}*/
+    catch(cb) {
+        this.onCatch = cb
+        return this
+    }
 
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//   еще про promise   еще про promise     еще про promise     еще про promise     еще про promise     еще про promise     еще про promise
+    finally(cb) {
+        this.onFinally = cb
+        return this
+    }
+}
 
-function sleep(ms) {
-    return new Promise((resolve) => {
-        setTimeout(function(){
-            resolve()
-        },ms)
+const promise = new MyPromise((resolve, reject) => {
+    setTimeout(() => {
+        reject('some error')
+        resolve(10)
+    }, 2000)
+
+
+})
+
+promise
+    .then(num => num *= 2)
+    .catch(err => console.error(err))
+    .then(num => num *= 3)
+    .finally(() => {
+        console.log('finally')
     })
-}
-
-sleep(1500).then(function() {
-    console.log('1.5')
-})
-sleep(5000).then(function() {
-    console.log('5')
-})
-
-// метод all принимает в себя набор промисов и отрабатывает тогда (попадает в метод .then), когда отработают все промисы которые он принял
-Promise.all([sleep(1500), sleep(5000)]).then(function(){
-    console.log('all')
-})
-
-// метод race принимает в себя набор промисов и отрабатывает тогда (попадает в метод .then), когда отрабатывает первый промис.
-Promise.race([sleep(1500), sleep(5000)]).then(function(){
-    console.log('race')
-})
-
-// более реальный пример
-let p1 = sleep(1500).then(function() {
-    return {
-        name: 'promise 1.5'
-    }
-})
-let p2 = sleep(5000).then(function() {
-    return {
-        name: 'promise 3'
-    }
-})
-
-Promise.all([p1, p2]).then(function(data){
-    console.log('all', data) /*  all (2) [{…}, {…}]
-                                        0: {name: "promise 1.5"}
-                                        1: {name: "promise 3"}
-                                        length: 2
-                                        __proto__: Array(0)
-                                         */
-})
-
-Promise.race([p1, p2]).then(function(data){
-    console.log('race', data) // race {name: "promise 1.5"}
-})
-
-
-async function start() {
-    let dataAll = await Promise.all([p1, p2])
-
-    let dataRace = await Promise.race([p1, p2])
-
-    console.log('dataAll', dataAll)
-    console.log('dataRace', dataRace)
-}
-
-start()
-
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//
-
-
+    .then(num => console.log('Done!', num))
