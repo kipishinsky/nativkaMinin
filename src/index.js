f4e136818ef1c481a394820d7f82e836abd0a6cf
SS007 <rbkmoneyukk@bk.ru>
2020-07-10 15:54:53 +0400

    refactoring

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './lesson1-2/index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
