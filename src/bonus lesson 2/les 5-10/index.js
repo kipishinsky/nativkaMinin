2bb7ecd0fd45516b31e4eb90c131d033712e75d5
SS007 <rbkmoneyukk@bk.ru>
2020-07-16 12:09:40 +0400

    lesson 10 node.js

---
+++ src/bonus lesson 2/les 5-10/index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+
+const bodeParser = require('bode-parser')
+const weatherRequest = require('./requests/weather_request.js')
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.get('/', (req, res) => {
+    res.render('index',  {weather: null, error: null})
+})
+
+app.post('/', async (req, res) => {
+    const {city} = req.body
+    const {weather, error} = await weatherRequest(city)
+    res.render('index', {weather, error})
+})
+
+app.listen (3000, () => {
+    console.log('server has started on port 3000');
+})
--- src/bonus lesson 2/les 5-10/requests/weather_request.js
+++ src/bonus lesson 2/les 5-10/requests/weather_request.js
@@ -1,11 +1,14 @@
-const rp = require('request-promise') // подключаем модуль
+const rp = require('request-promise')
 
 module.exports = async function(city = '') {
+
     if(!city) {
         throw new Error ('Имя города не может быть пустым')
     }
+
     const KEY = '124134h13jg12g312h3j'
     const uri = 'https://api.openweathermap.org/data/2.5/weather'
+
     const options = {
         uri,
         qs: {
@@ -13,26 +16,23 @@ module.exports = async function(city = '') {
             q: city,
             units: 'imperial'
         },
+
         json: true
     }
 
     try {
         const response = await rp(options)
-/*        console.log(response)*/
-        const celsium = (data.main.temp - 32) * 5/9 // переводим в цельсии, формула (  (data.main.temp - 32) * 5/9    )
+        const celsium = (data.main.temp - 32) * 5/9
 
         return {
-            /*weather: '',*/
             weather: `${data.name} : ${celsium.toFixed(0)}`, // санкт петербург 23
             error: null
         }
     } catch (error) {
-/*        console.log(error)*/
         return {
             weather: null,
             error: error.error.message
         }
     }
-    /*const response = await rp(options)
-    console.log(response)  // { много параметров }*/
+
 }
