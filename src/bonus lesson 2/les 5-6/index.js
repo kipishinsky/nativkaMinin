06b3fe804ba9869de29ed44d8a28b11a047b6b3f
SS007 <rbkmoneyukk@bk.ru>
2020-07-16 11:03:09 +0400

    lesson 6 node.js

--- src/bonus lesson 2/les 5-6/index.js
+++ src/bonus lesson 2/les 5-6/index.js
@@ -4,15 +4,24 @@
 // потом
 const express = require('express')
 const app = express()
+const bodeParser = require('bode-parser')
 
 app.set('view engine', 'ejs') // default files ejs
 app.use(express.static('public')) /*
  use - позволяет использовать дополнительные опции в нашем приложении
  static - говорит об использовании статического пути
  */
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.get('/', (req, res) => { // методом get
     res.render('index') // мы будем рендерить некий файл индекс, с расширение ejs
+})
+
+app.post('/', (req, res) => { /*  req - то с чем обращаемся в данный запрос, res - то как мы отвечаем на данный запрос */
+    /* console.log(req.params) - отвечает за квери запрос после ? в адресной строке */
+    const {city} = req.body
+    console.log(city) // Санкт Петербург
+    res.render('index')
 })
 
 app.listen (3000, () => { // слушаем 3000 порт
